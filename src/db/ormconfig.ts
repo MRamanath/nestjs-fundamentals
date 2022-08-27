a15376e942f6663ef229e1563e0470a8c358c73b
Ramanath <ramanath.mandal@codeclouds.in>
2022-08-28 02:19:42 +0530

    migrations

---
+++ src/db/ormconfig.ts
@@ -0,0 +1,18 @@
+import { Coffee } from 'src/coffees/entities/coffee.entity';
+import { Flavor } from 'src/coffees/entities/flavor.entity';
+import { Event } from 'src/events/entities/event.entity';
+import { DataSource } from 'typeorm';
+
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'postgres',
+  entities: [Event, Coffee, Flavor],
+  migrationsTableName: 'migrations',
+  migrations: ['src/db/migrations/*.ts'],
+});
+
+export default dataSource;
